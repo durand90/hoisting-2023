78280f177bbabcc8c40cfdd11bab2e001b8a2ab1
Fanfan durand <durand90@live.com>
2023-05-03 19:59:56 -0400

    three

--- hoisting.js
+++ hoisting.js
@@ -4,11 +4,29 @@ console.log(example);
 let example = "I'm the example!";
 */
 
-// 1
+/*
+// 1     undefined
 console.log(hello);
 var hello = 'world';
 //after Hoisting
 //var
 //console.log(hello);
 //hello = 'world'
+*/
 
+
+//2    reference error
+//var needle = 'haystack';
+test();
+function test(){
+    var needle = 'magnet';
+    console.log(needle);
+}
+//after Hoisting
+// var 
+//function test()
+//var
+//var needle = 'haystack'
+//function test()
+//var needle = 'magnet'
+//console.log(needle)
