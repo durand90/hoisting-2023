7b735080ca054bfeb82b764fa9743f3aed7d9314
Fanfan durand <durand90@live.com>
2023-05-03 19:53:21 -0400

    second

---
+++ hoisting.js
@@ -0,0 +1,14 @@
+
+/*
+console.log(example);
+let example = "I'm the example!";
+*/
+
+// 1
+console.log(hello);
+var hello = 'world';
+//after Hoisting
+//var
+//console.log(hello);
+//hello = 'world'
+
